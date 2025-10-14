95090b568f9edf62ae2b0608bc7f70e13d10b1f6
Jere1203 <jeremias.bartolsic@hotmail.com>
2025-10-14 19:56:01 -0300

    Ej20

--- EjsSQL.sql
+++ EjsSQL.sql
@@ -386,3 +386,34 @@ having p1.prod_familia <>
                             order by count(*) desc
                         )
 order by p1.prod_detalle asc
+
+-- 20. Escriba una consulta sql que retorne un ranking de los mejores 3 empleados del 2012
+-- Se debera retornar legajo, nombre y apellido, anio de ingreso, puntaje 2011, puntaje
+-- 2012. El puntaje de cada empleado se calculara de la siguiente manera: para los que
+-- hayan vendido al menos 50 facturas el puntaje se calculara como la cantidad de facturas
+-- que superen los 100 pesos que haya vendido en el año, para los que tengan menos de 50
+-- facturas en el año el calculo del puntaje sera el 50% de cantidad de facturas realizadas
+-- por sus subordinados directos en dicho año.
+
+select empl_codigo, empl_nombre, empl_apellido, year(empl_ingreso)'INGRESO',
+(
+    select
+    CASE WHEN count(distinct fact_numero) >= 50
+    THEN (select count(*) from Factura where fact_vendedor = empl_codigo and year(fact_fecha) = 2011 and fact_total > 100)
+    ELSE (select count(*)/2 from Factura join Empleado e2 on fact_vendedor = e2.empl_codigo and e2.empl_jefe = empl_codigo and YEAR(fact_fecha) = 2011 group by fact_vendedor)
+    END
+    from Factura
+    where fact_vendedor = empl_codigo
+    group by fact_vendedor
+) 'Puntaje 2011',
+(
+    select
+    CASE WHEN count(distinct fact_numero) >= 50
+    THEN (select count(*) from Factura where fact_vendedor = empl_codigo and year(fact_fecha) = 2012 and fact_total > 100)
+    ELSE (select count(*)/2 from Factura join Empleado e2 on fact_vendedor = e2.empl_codigo and e2.empl_jefe = empl_codigo and YEAR(fact_fecha) = 2012 group by fact_vendedor)
+    END
+    from Factura
+    where fact_vendedor = empl_codigo
+    group by fact_vendedor
+)
+from Empleado
