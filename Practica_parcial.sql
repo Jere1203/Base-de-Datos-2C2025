9cdd3ec5f89118a7b21c31c652890cd601501a07
Jere1203 <jeremias.bartolsic@hotmail.com>
2025-11-03 19:14:02 -0300

    Agrego ejs de parcial

---
+++ Practica_parcial.sql
@@ -0,0 +1,103 @@
+/* 1. Realizar una consulta SQL que retorne para el último año, los 5 vendedores con menos clientes asignados, 
+que más vendieron en pesos (si hay varios con menos clientes asignados debe traer el que más vendió), solo deben 
+considerarse las facturas que tengan más de dos ítems facturados:
+
+1)	Apellido y Nombre  del Vendedor.
+2)	Total de unidades de Producto Vendidas.
+3)	Monto promedio de venta por factura.
+4)	Monto total de ventas.
+
+El resultado deberá mostrar ordenado la cantidad de ventas descendente, en caso de igualdad de cantidades, 
+ordenar por código de vendedor.
+NOTA: No se permite el uso de sub-selects en el FROM. */
+
+select fact_vendedor, sum(item_cantidad), avg(fact_total), sum(item_cantidad*item_precio)
+from Factura join Item_Factura on item_sucursal+item_tipo+item_numero=fact_sucursal+fact_tipo+fact_numero
+where fact_vendedor in 
+                        (
+                            select top 5 f.fact_vendedor
+                            from Factura f
+                            group by f.fact_vendedor
+                            order by count(distinct f.fact_cliente) asc
+                        )
+group by fact_vendedor
+having count(item_cantidad) > 2
+order by count(fact_numero) desc
+
+-- Creo que esta es la que va
+select top 5 empl_nombre, empl_apellido, avg(fact_total), sum(item_cantidad*item_precio)
+from Empleado join Factura on fact_vendedor = empl_codigo and year(fact_fecha) = (select max(year(fact_fecha)) from Factura)
+join Item_Factura on fact_tipo+fact_numero+fact_sucursal=item_tipo+item_numero+item_sucursal
+group by empl_nombre, empl_apellido, empl_codigo
+order by (select count(*) from Cliente where clie_vendedor = empl_codigo) ASC, count(fact_numero) desc, empl_codigo
+
+
+
+/* 2. Dado el contexto inflacionario se tiene que aplicar un control en el cual nunca se permita vender un producto a un 
+precio que no esté entre 0%-5% del precio de venta del producto el mes anterior, ni tampoco que esté en más de un 50% el 
+precio del mismo producto que hace 12 meses atrás. Aquellos productos nuevos, o que no tuvieron ventas en meses anteriores 
+no debe considerar esta regla ya que no hay precio de referencia. */
+GO
+create trigger ej_parcial on Item_Factura for INSERT
+as
+BEGIN
+    declare @prod char(8), @prec decimal(12,2)
+    declare c_vendidos cursor for (select i.item_producto, i.item_precio from inserted i where i.item_producto not in (select item_producto from Item_Factura))
+    open c_vendidos
+    fetch c_vendidos into @prod
+    while @@FETCH_STATUS = 0
+    BEGIN
+        IF (@prec - (select item_precio from Item_Factura join Factura on item_tipo+item_sucursal+item_numero=fact_tipo+fact_sucursal+fact_numero where item_producto = @prod and MONTH(fact_fecha) = month(fact_fecha)-1))*100 BETWEEN 0 and 5
+        BEGIN
+            IF (@prec - (select item_precio from Item_Factura join Factura on item_tipo+item_numero+item_sucursal=fact_tipo+fact_numero+fact_sucursal where item_producto=@prod and year(fact_fecha)=YEAR(fact_fecha)-1))*100 > 50
+            BEGIN
+                PRINT 'No se puede vender a este precio'
+            END
+        END
+    END
+END
+
+
+GO
+create function noSePuedeVender (@precio decimal(12,2), @producto char(8))
+returns INT
+BEGIN
+    declare @precioMesAnterior decimal(12,2), @precioAnioPasado decimal(12,2)
+    set @precioMesAnterior = (select item_precio from Item_Factura join Factura on item_tipo+item_numero+item_sucursal=fact_tipo+fact_numero+fact_sucursal where month(fact_fecha) = month(fact_fecha)-1 and item_producto = @producto)
+    set @precioAnioPasado = (select item_precio from Item_Factura join Factura on item_tipo+item_numero+item_sucursal=fact_tipo+fact_numero+fact_sucursal where year(fact_fecha) = year(fact_fecha)-1 and item_producto = @producto)
+    IF ABS(@precio - @precioAnioPasado) > 0.5*@precioAnioPasado or ABS(@precio - @precioMesAnterior) BETWEEN 0.05*@precioMesAnterior and 0
+    BEGIN
+        return 1
+    END
+    return 0
+END
+
+
+GO
+create trigger ej_parcialv2 on Item_factura for INSERT
+as
+BEGIN
+    IF (select count(*) 
+        from inserted i 
+        join Factura on i.item_tipo+i.item_numero+i.item_sucursal=fact_tipo+fact_numero+fact_sucursal
+        where i.item_producto in (
+            select item_producto
+            from Item_Factura join Factura on item_tipo+item_numero+item_sucursal=fact_tipo+fact_numero+fact_sucursal
+            where year(fact_fecha) = year(fact_fecha)-1
+        )
+        and i.item_producto in (
+            select item_producto
+            from Item_Factura join Factura on item_tipo+item_numero+item_sucursal=fact_tipo+fact_numero+fact_sucursal
+            where month(fact_fecha) = month(fact_fecha)-1
+        ))>0
+    BEGIN
+        IF (select count(*) from inserted i join Factura on i.item_tipo+i.item_numero+i.item_sucursal=fact_tipo+fact_numero+fact_sucursal
+            where dbo.noSePuedeVender(i.item_precio, i.item_producto)=1) > 0
+            BEGIN
+                PRINT 'No se cumple la regla'
+                ROLLBACK TRANSACTION
+            END
+    END
+END
+
+
